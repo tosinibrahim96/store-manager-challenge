594322d453208cc931222f22a79120d94af7ff78
ibrahim <tosinibrahim96@gmail.com>
2018-10-10 02:58:55 +0100

    Feature: Sidebar added for admin

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-	$(".admin-box").hide();
-	$("#show").click(function() {
-		$(".attendant-box").hide();
-		$(".admin-box").fadeIn(200);
-	});
-	$("#hide").click(function() {
-		$(".attendant-box").fadeIn(200);
-		$(".admin-box").hide();
-	});
+const sidebar = document.querySelector(".toggle-btn");
+
+$(".admin-box").hide();
+$("#show").click(function() {
+	$(".attendant-box").hide();
+	$(".admin-box").fadeIn(200);
 });
+$("#hide").click(function() {
+	$(".attendant-box").fadeIn(200);
+	$(".admin-box").hide();
+});
+
+sidebar.addEventListener("click", toggleSidebar);
+toggleSidebar = () =>
+	document.getElementById("sidebar").classList.toggle("active");

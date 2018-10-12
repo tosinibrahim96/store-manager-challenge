81a81b4e7d367034a1aaff56a716708a005f2181
ibrahim <tosinibrahim96@gmail.com>
2018-10-12 01:49:21 +0100

    Attendant dashboard begins here

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const togggleButton = document.querySelector(".toggle-btn");
 const sidebar = document.getElementById("sidebar");
+let input, filter, table, tr, td, i;
+input = document.getElementById("myInput");
 
 $(".admin-box").hide();
 $("#show").click(function() {
@@ -16,4 +18,24 @@ toggleSidebar = () => {
 	sidebar.classList.toggle("show");
 };
 
+searchProduct = () => {
+	// Declare variables
+	filter = input.value.toUpperCase();
+	table = document.querySelector(".searchTable");
+	tr = table.getElementsByTagName("tr");
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				tr[i].style.display = "";
+			} else {
+				tr[i].style.display = "none";
+			}
+		}
+	}
+};
+
 togggleButton.addEventListener("click", toggleSidebar);
+input.addEventListener("keyup", searchProduct);
